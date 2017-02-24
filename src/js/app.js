fb4479f10e2d602307e7455b3a4beeb32eef3966
Serhii <kso_132@mail.ru>
2017-02-24 02:01:35 +0200

    mobile btn ready

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,29 @@
 //=include lib/svgxuse.min.js
 
 sayHello();
+$(document).ready(function(){
+	var windowWidth = $(window).width();
+
+	$(window).resize(function() {
+		windowWidth = $(window).width();
+	});
+
+	$('.slider').slick({
+		infinite: true,
+		arrows: true
+	});
+
+	$(".menu-btn").on("click", function(){
+		var _this = $(this);
+		_this.toggleClass("menu-btn-is-active");
+		$("body").addClass("body-ovh");
+		$(".js-nav-responsive").slideToggle(300);
+		if(windowWidth <= 991 && windowWidth >= 768){
+			$(".js-hidden").toggleClass("is-hidden");
+		};
+	});
+
+
+
+	//ready
+});
